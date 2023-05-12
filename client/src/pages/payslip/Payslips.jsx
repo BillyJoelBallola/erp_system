ff4929e5fde801b829f153eac7e7879c7aaa1420
BillyJoelBallola <billyjoelballola27@gmail.com>
2023-05-12 13:26:12 +0800

    delete unused package

--- client/src/pages/payslip/Payslips.jsx
+++ client/src/pages/payslip/Payslips.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Table from "../../components/Table";
 import axios from "axios";
 
